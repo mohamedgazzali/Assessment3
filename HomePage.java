005c106b07970c575f7abe3af57ed42add726e70
mohamedgazzali <mohamedgazzali.gk@gmail.com>
2022-09-13 10:53:13 +0530

    Add files via upload

---
+++ HomePage.java
@@ -0,0 +1,15 @@
+package com.leaftaps.pages;
+
+import com.framework.selenium.api.design.Locators;
+import com.framework.testng.api.base.ProjectSpecificMethods;
+
+public class HomePage extends ProjectSpecificMethods{
+	
+	public SelectPage verifyHomePage() {
+		verifyDisplayed(locateElement(Locators.CLASS_NAME, "uiOutputText"));
+		reportStep("Homepage is loaded", "pass");
+		return new SelectPage();
+	}
+	
+	
+}
